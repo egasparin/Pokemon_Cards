4574495959ae430053de70fdfc8415abf0635a54
eduardo <eduardo@debian>
2025-01-01 11:19:59 -0300

    Correção do arquivo de configuração

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["www.pokemon.com"],
+    // domains: ["www.pokemon.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.pokemon.com",
+        port: "", // Deixe vazio se não houver uma porta específica.
+        pathname: "/**", // Permite todos os caminhos do domínio.
+      },
+    ],
   },
 };
 
